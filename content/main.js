677692b105f1fd3eb79653b08156d77a53d88510
David Schulte <mail@david-schulte.de>
2023-08-25 19:03:09 +0200

    Sync volume settings across all videos on a page when viewing a feed.

--- content/main.js
+++ content/main.js
@@ -1,16 +1,92 @@
+const registeredVideos = [];
+
+let playbackVolume = 0.0;
+let playbackMuted = true;
+
+// Is called when a new video element was detected on the page.
+function registerVideoElement(video) {
+    // Enable Html controls
+    video.controls = true;
+
+    // Update volume
+    updateVolumeForVideo(video);
+
+    // Remove Instagram controls and the pause handler.
+    const elementAfterVideo = video.nextElementSibling;
+    if (elementAfterVideo) {
+        elementAfterVideo.remove();
+    }
+
+    video.addEventListener("volumechange", onVolumeChanged);
+    video.addEventListener("play", onPlay);
+}
+
+// Is called when a video element was removed from the page.
+function unregisterVideoElement(video) {
+    video.removeEventListener("volumechange", onVolumeChanged);
+    video.removeEventListener("play", onPlay);
+}
+
+
+// Applies the stored volume to all registered videos.
+function updateVolumeForVideos() {
+    for (const video of registeredVideos) {
+        updateVolumeForVideo(video);
+    }
+}
+
+// Applies the stored volume to the given video.
+function updateVolumeForVideo(video) {
+    video.volume = playbackVolume;
+    video.muted = playbackMuted;
+}
+
+// Is called when the volume was changed of any registered video.
+function onVolumeChanged(event) {
+    // We don't want to react to volume changes from the page itself.
+    if (!event.isTrusted) return;
+
+    const video = event.target;
+    playbackVolume = video.volume;
+    playbackMuted = video.muted;
+
+    // Sync the volume across all other video players.
+    updateVolumeForVideos();
+}
+
+// Is called when a video is starting playback.
+function onPlay(event) {
+    const video = event.target;
+
+    // Make sure we apply the last used volume settings.
+    updateVolumeForVideo(video);
+}
+
+// Checks for new video elements on the page.
 function checkForVideosAndEnableHtmlControls() {
     const videos = document.getElementsByTagName('video');
+
+    // Detect new videos...
     for (const video of videos) {
-        if (video.controls) continue;
+        if (registeredVideos.includes(video)) continue;
+        registeredVideos.push(video);
+        registerVideoElement(video);
+    }
 
-        // Enable Html controls
-        video.controls = true;
-
-        // Remove Instagram controls and the pause handler.
-        const elementAfterVideo = video.nextElementSibling;
-        if (elementAfterVideo) {
-            elementAfterVideo.remove();
+    // Detect removed videos...
+    for (let i = 0; i < registeredVideos.length; i++) {
+        const video = registeredVideos[i];
+        let found = false;
+        for (let n = 0; n < videos.length; n++) {
+            if (videos[n] === video) {
+                found = true;
+                break;
+            }
         }
+        if (found) continue;
+        registeredVideos.splice(i, 1);
+        unregisterVideoElement(video);
+        i--;
     }
 }
 
