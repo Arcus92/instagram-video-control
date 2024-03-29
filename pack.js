3fae296fab3ef83c4e5c0723f85d2379ed3870cc
David Schulte <mail@david-schulte.de>
2024-03-29 17:24:26 +0100

    Defined project structure for settings popup and updated icons.

--- pack.js
+++ pack.js
@@ -5,7 +5,7 @@ const childProcess = require('child_process');
 
 const distPath = path.join(__dirname, 'dist');
 const sourcePath = path.join(__dirname, 'dist', 'js');
-const staticPath = path.join(__dirname, 'content', 'static');
+const staticPath = path.join(__dirname, 'src', 'static');
 
 // Getting the version from the package.json
 const versionBuffer = childProcess.execSync("npm pkg get version --parseable", { encoding: 'utf8' });
@@ -16,8 +16,9 @@ const platformNames = ['firefox', 'chrome'];
 for (const platformName of platformNames) {
     console.log(`Packing plugin for ${platformName}...`);
     const platformDistPath = path.join(distPath, platformName);
+    const platformSourceDistPath = path.join(distPath, platformName, 'js');
     const platformDistManifestPath = path.join(distPath, platformName, 'manifest.json');
-    const platformManifestPath = path.join(__dirname, 'content', 'manifest', `${platformName}.json`);
+    const platformManifestPath = path.join(__dirname, 'src', 'manifest', `${platformName}.json`);
     const platformZipPath = path.join(distPath, platformName + '.zip');
 
     // Clear output directory
@@ -29,7 +30,7 @@ for (const platformName of platformNames) {
 
     // Copy content. TypeScript must be compiled by now.
     fs.cpSync(staticPath, platformDistPath, { recursive: true });
-    fs.cpSync(sourcePath, platformDistPath, { recursive: true });
+    fs.cpSync(sourcePath, platformSourceDistPath, { recursive: true });
 
     // Copy manifest and replacing the version.
     let manifest = fs.readFileSync(platformManifestPath, 'utf8');
